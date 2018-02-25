4696359d4094c2d6c7494baf634ec687e85ab108
sh-tatsuno <sh-tatsuno@tatsunoshounoMacBook-Pro.local>
2018-02-25 23:20:42 +0900

    refine to fix error

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ def migrate():
     os.system("truffle migrate")
     return '', 200
 
-@app.route
+@app.route('/balance')
+def balance(): # to be continued
+    return '', 200
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
