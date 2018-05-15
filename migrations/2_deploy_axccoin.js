316104ee03cce3e6a0bc038bb6c0c0c21eb32a18
里見健太 <satomikennta1213@gmail.com>
2018-05-15 16:32:09 +0900

    change some npm pcks

--- migrations/2_deploy_axccoin.js
+++ migrations/2_deploy_axccoin.js
@@ -1,6 +1,5 @@
 var AXCToken = artifacts.require('./AXCToken.sol')
 var AXCCrowdsale = artifacts.require('./AXCCrowdsale.sol')
-
 const duration = {
     seconds: function(val) { return val},
     minutes: function(val) { return val * this.seconds(60) },
@@ -13,10 +12,10 @@ const duration = {
 const promisefy = (fn, ...args) => new Promise((accept, reject) => fn(...args, (err, res) => err ? reject(err) : accept(res)))
 
 module.exports = function(deployer, network, accounts) {
-  // const timestamp = (await promisefy(web3.eth.getBlock, 'latest')).timestamp
-  const rate = new web3.BigNumber(1000); // trade rate from ETH
-  const goal = web3.toWei(10000, 'ether');
-  const cap = web3.toWei(1000000 , 'ether');
+  //const timestamp = (await promisefy(web3.eth.getBlock, 'latest')).timestamp
+  const rate = new web3.BigNumber(10000); // trade rate from ETH
+  const goal = web3.toWei(100000, 'ether');
+  const cap = web3.toWei(10000000, 'ether');
   const tokencap = web3.toWei(10000000 , 'ether');
   const wallet = accounts[0];
 
@@ -24,13 +23,14 @@ module.exports = function(deployer, network, accounts) {
         .then(async () => {
             return deployer.deploy(
               AXCToken,
-              tokencap,{gas: 2000000}
+              tokencap,{gas:2000000}
             );
         })
         .then(async () => {
           const timestamp = (await promisefy(web3.eth.getBlock, 'latest')).timestamp;
-          const openingTime = timestamp + duration.minutes(1);
-          const closingTime = openingTime + duration.days(10);
+          const openingTime = timestamp + duration.minutes(15);
+          // const openingTime = timestamp + 120;
+          const closingTime = openingTime + duration.minutes(75);
           return deployer.deploy(
               AXCCrowdsale,
               openingTime,
